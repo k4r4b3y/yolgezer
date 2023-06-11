cdda4b143fb0151d5fe299d43e868a8a0a875e37
k4r4b3y <128297497+k4r4b3y@users.noreply.github.com>
2023-06-11 16:11:02 +0300

    doing what the ai says

--- src/yolgezer.sh
+++ src/yolgezer.sh
@@ -50,8 +50,12 @@ mkdir -p ${xmr_binary_dir}
 sed -i 's/^socks4.*127.0.0.1 9050$/socks5 127.0.0.1 9055/' ${PREFIX}/etc/proxychains.conf
 proxychains4 -q wget -q --show-progress -O "${xmr_binary_dir}/android_monero_binaries" "${xmr_dl_onion}"
 
-7z x "${xmr_binary_dir}/android_monero_binaries" -so | 7z x -aoa -si -ttar -spe -o"${xmr_binary_dir}"
-chmod +x ${xmr_binary_dir}/monero*
+#7z x "${xmr_binary_dir}/android_monero_binaries" -so | 7z x -aoa -si -ttar -o"${xmr_binary_dir}"
+#chmod +x ${xmr_binary_dir}/monero*
+7z x "${xmr_binary_dir}/android_monero_binaries" -so | (
+  mv android_monero_binaries/* ${xmr_binary_dir}/
+) && 7z x -aoa -si -ttar -o"${xmr_binary_dir}"
+
 
 # create the config file for the monero daemon
 #
