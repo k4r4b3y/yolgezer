494ef6dc3b3a59fcf27943e4fb516e85eb47e94b
k4r4b3y <128297497+k4r4b3y@users.noreply.github.com>
2023-06-11 13:46:40 +0300

    make monero bins executable

--- src/yolgezer.sh
+++ src/yolgezer.sh
@@ -47,9 +47,9 @@ esac
 # use torsocks -P 9055
 mkdir -p ${xmr_binary_dir}
 cd ${xmr_binary_dir}
-torsocks -P 9055 wget "${xmr_dl_onion}" -O "android_monero_binaries"
+torsocks -P 9055 wget "${xmr_dl_onion}" -O android_monero_binaries
 7z x "android_monero_binaries" -so | 7z x -aoa -si -ttar
-
+chmod +x monero-*/monero*
 
 # create the config file for the monero daemon
 #
